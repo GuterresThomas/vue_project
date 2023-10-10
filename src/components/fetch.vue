39a58aaa339caf03177f7e59f548a5e8b9ff93ef
Guterresthomas <guterresmikael@gmail.com>
2023-10-10 16:54:21 -0300

    vue_fetch

---
+++ src/components/fetch.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
+
+
+
+    const todos = ref<any>(null);
+
+
+    async function fetchData() {
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos');
+            const data = await response.json();
+            todos.value = data;
+        }catch (error) {
+            console.log(error)
+        }
+    }
+
+    onMounted(() => {
+        fetchData()
+    })
+
+
+</script>
+
+<template>
+    <div>
+       <ul>
+        <li v-for="todo in todos" :key="todo.id">
+            <div>{{ todo.title }}</div>
+        </li>
+       </ul>
+    </div>
+
+</template>
